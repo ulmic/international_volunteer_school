4fc49bf577368a45a4ee8e7d58b27203c8c933ed
pavel <kalashnikovisme@gmail.com>
2015-06-28 18:19:05 +0300

    add counts to navbar

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,4 +1,12 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
   before_filter :authenticate_admin!
+  before_filter :counts
   layout 'web/admin/application'
+
+  private
+
+  def counts
+    @organizers_count = Organizer.count
+    @participants_count = Participant.count
+  end
 end
