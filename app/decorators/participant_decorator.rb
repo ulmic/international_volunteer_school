b257d75ae189c3b0f55d9ea445235d94c29f1e7d
pavel <kalashnikovisme@gmail.com>
2015-07-24 12:45:20 +0300

    place decorate method

--- app/decorators/participant_decorator.rb
+++ app/decorators/participant_decorator.rb
@@ -1,3 +1,11 @@
 class ParticipantDecorator < UserDecorator
   delegate_all
+
+  def place
+    if region == 'Ульяновская область'
+      "#{object.municipality} #{object.locality}"
+    else
+      object.region
+    end
+  end
 end
